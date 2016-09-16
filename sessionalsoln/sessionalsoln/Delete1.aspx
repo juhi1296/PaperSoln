8df711c26bda23c934bba7ec4ac7ff8f338d0dbf
Aditi-0304 <aditi3april@gmail.com>
2016-09-16 12:40:31 +0530

    DELETE PAGE CREATED

---
+++ sessionalsoln/sessionalsoln/Delete1.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Delete1.aspx.cs" Inherits="sessionalsoln.Delete1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+     <asp:Label ID="Label1" runat="server" Text="ID"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ sessionalsoln/sessionalsoln/Delete1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+namespace sessionalsoln
+{
+    public partial class Delete1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+              SqlCommand cmd = new SqlCommand();
+            SqlConnection 
+                SqlConnection con = new SqlConnection(connectionString);
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
